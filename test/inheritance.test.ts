41d14c4a1ada50f0b273299e7fb172e1d0cf38e5
Hilmi AM <imiia75775@proton.me>
2024-08-30 02:36:02 +0700

    inheritance

---
+++ test/inheritance.test.ts
@@ -0,0 +1,28 @@
+import {name} from "ts-jest/dist/transformers/hoist-jest";
+
+describe('Inheritance', () => {
+    class Employee {
+        name: string;
+
+        constructor(name: string) {
+            this.name = name;
+        }
+    }
+
+    class Manager extends Employee {
+    }
+
+    class Director extends Employee {
+    }
+
+    it('should inherit', () => {
+        const employe = new Employee("MEE");
+        console.info(employe.name)
+
+        const manag = new Manager("ZEE");
+        console.info(manag.name)
+
+        const direc = new Director("HMM");
+        console.info(direc.name)
+    });
+});
