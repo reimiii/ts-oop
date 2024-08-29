4d799033c1fa3defbdbed201b3761b27232a178c
Hilmi AM <imiia75775@proton.me>
2024-08-30 01:47:34 +0700

    class

---
+++ test/class.test.ts
@@ -0,0 +1,13 @@
+describe('Class', () => {
+    it('should create class', () => {
+
+        class Customer {
+        }
+
+        class Order {
+        }
+
+        const customer: Customer = new Customer();
+        const order = new Order();
+    });
+});
