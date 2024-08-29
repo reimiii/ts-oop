c314ac85aefaaa87e4dd64d6f9320b50b6edce17
Hilmi AM <imiia75775@proton.me>
2024-08-30 02:50:22 +0700

    inheritance data and method impl

---
+++ test/interface.test.ts
@@ -0,0 +1,28 @@
+import {name} from "ts-jest/dist/transformers/hoist-jest";
+
+describe('Interface can be data or method', () => {
+    interface HasName {
+        name: string;
+    }
+
+    interface CanSayHi {
+        sayHi(name: string): void;
+    }
+
+    class Person implements HasName, CanSayHi {
+        name: string;
+
+        constructor(name: string) {
+            this.name = name;
+        }
+
+        sayHi(name: string): void {
+            console.info(`hi ${name}, my name is ${this.name}`);
+        }
+    }
+
+    it('should support data and method', () => {
+        const person: Person = new Person("Mee");
+        person.sayHi("Pacman");
+    });
+});
