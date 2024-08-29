1b3b0621cca5f27cbf2a4a66c16341b6b3aea422
Hilmi AM <imiia75775@proton.me>
2024-08-30 03:02:15 +0700

    parent class constructor

---
+++ test/super-constructor.test.ts
@@ -0,0 +1,26 @@
+import {name} from "ts-jest/dist/transformers/hoist-jest";
+
+describe('Super Class keyword', () => {
+    class Person {
+        name: string;
+
+        constructor(name: string) {
+            this.name = name;
+        }
+    }
+
+    class Employee extends Person {
+        department: string;
+
+        constructor(name: string, department: string) {
+            super(name);
+            this.department = department;
+        }
+    }
+
+    it('should support parent constructor', () => {
+        const emp = new Employee("Hilmi", "IT");
+        console.info(emp.name);
+        console.info(emp.department);
+    });
+});
