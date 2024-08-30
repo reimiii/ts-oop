32e9cc412849f42e33d3beea7a5f05df09233f08
Hilmi AM <imiia75775@proton.me>
2024-08-30 15:07:35 +0700

    namespace

---
+++ src/utility.ts
@@ -0,0 +1,6 @@
+export namespace Utility {
+    export const PI: number = 3.14;
+}
+
+export namespace Me {}
+export namespace Ze {}
---
+++ test/namespace.test.ts
@@ -0,0 +1,7 @@
+import {Utility} from "../src/utility";
+
+describe('Namespace', () => {
+    it('should support', () => {
+        console.info(Utility.PI)
+    });
+});
