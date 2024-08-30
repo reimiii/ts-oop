89cd5bf67d7e1ea55a7dbaaac98e55626907bd0f
Hilmi AM <imiia75775@proton.me>
2024-08-30 12:44:53 +0700

    type cast wrong

--- test/polymorphism.test.ts
+++ test/polymorphism.test.ts
@@ -22,6 +22,18 @@ describe('Polymorphism', () => {
         }
     }
 
+    function sayHiWrong(emp: Employee): void {
+        if (emp instanceof Manager) {
+            const man = emp as Manager;
+            console.info(`hi manager ${man._name}`);
+        } else if (emp instanceof VicePresident) {
+            const vp = emp as VicePresident;
+            console.info(`hi vp ${vp._name}`);
+        } else {
+            console.info(`hi employee ${emp._name}`);
+        }
+    }
+
     it('should support polymorphism', () => {
         let emp: Employee = new Employee("hilmi");
         console.info(emp);
@@ -37,5 +49,11 @@ describe('Polymorphism', () => {
         sayHi(new Employee("Mee"));
         sayHi(new Manager("001"));
         sayHi(new VicePresident("002"));
+    });
+
+    it('should wrong type cast ', () => {
+        sayHiWrong(new Employee("Mee"));
+        sayHiWrong(new Manager("001"));
+        sayHiWrong(new VicePresident("002"));
     });
 });
