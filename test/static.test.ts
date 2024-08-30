37bc8ad1bea905ff0c1bc2638198daf225718628
Hilmi AM <imiia75775@proton.me>
2024-08-30 13:48:19 +0700

    static method

--- test/static.test.ts
+++ test/static.test.ts
@@ -4,6 +4,21 @@ describe('Static', () => {
         static KEY: string = "SECRET";
     }
 
+    class Utils {
+        static sum(...v: number[]): number {
+            let t = 0;
+            for (const n of v) {
+                t += n;
+            }
+
+            return t;
+        }
+    }
+
+    it('should static method', () => {
+        console.info(Utils.sum(1, 2, 3, 4, 5));
+    });
+
     it('should support properties', () => {
         console.info(Conf.KEY);
         console.info(Conf.VERSION);
