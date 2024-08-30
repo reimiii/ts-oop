a11f55ade8db18b682e4a4f8ca760be143234ccc
Hilmi AM <imiia75775@proton.me>
2024-08-30 11:10:49 +0700

    method overriding

---
+++ test/method-overriding.test.ts
@@ -0,0 +1,30 @@
+import {name} from "ts-jest/dist/transformers/hoist-jest";
+
+describe('Method Overriding', () => {
+    class Employee {
+        name: string;
+
+        constructor(name: string) {
+            this.name = name;
+        }
+
+        sayHi(name: string): void {
+            console.info(`hi ${name}, my name is ${this.name}`);
+        }
+    }
+
+    class Manager extends Employee {
+
+        sayHi(name: string) {
+            console.info(`hi ${name}, my name is ${this.name}. im u manager...`);
+        }
+    }
+
+    it('should overriding', () => {
+        const emp = new Employee("Yuna");
+        emp.sayHi("Rim");
+
+        const man = new Manager("Beni");
+        man.sayHi("Yuna");
+    });
+});
